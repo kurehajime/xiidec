2989b05666da6fd8f0781d872fee4abbad14fbe0
kurehajime <x.iidec+gh@googlemail.com>
2016-06-03 21:38:35 +0900

    page speed改善

--- static/colamone_beta/jq_option.js
+++ static/colamone_beta/jq_option.js
@@ -1,2 +1,1 @@
-/* @license Copyright (c) @kurehajime / source code: https://github.com/kurehajime/colamone_js */
-$(document).bind('mobileinit',function(){$.mobile.pushStateEnabled = false;});
+$(document).bind("mobileinit",function(){$.mobile.pushStateEnabled=!1});
