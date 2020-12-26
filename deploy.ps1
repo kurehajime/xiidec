fe457f63b78fa893f878378e96abbc7d89ca3a55
kurehajime <xiidec@gmail.com>
2020-12-26 20:39:18 +0900

    min_instancesを削除

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
 cd $PSScriptRoot
 robocopy ../colamone_js/docs/ ./static/colamone /s
-echo yes|gcloud app deploy
+echo yes|gcloud app deploy -v xyz
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 cd `dirname $0`
 cp ../colamone_js/docs/* ./static/colamone
-yes|gcloud app deploy
+yes|gcloud app deploy -v xyz
